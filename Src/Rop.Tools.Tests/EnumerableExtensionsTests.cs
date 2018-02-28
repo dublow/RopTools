0e58b0086673e8754b6464180d1ddac68a8fd407
Johann <johann.vanbeveren@gmail.com>
2018-02-28 13:58:57 +0100

    RedTest: ShouldFlattenOnlyItemOfTypeSomeAndReturnFlattenedSequence

---
+++ Src/Rop.Tools.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Rop.Tools.Tests
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void ShouldFlattenOnlyItemOfTypeSomeAndReturnFlattenedSequence()
+        {
+            var evens = new[] { 2, 4, 6 };
+
+            var actual = evens.Flatten(x => x
+                .When(y => (y % 2) == 0)
+                .Map(z => true));
+
+            Check.That(actual).InheritsFrom<IEnumerable<bool>>();
+
+        }
+    }
+}
---
+++ Src/Rop.Tools/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rop.Tools
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Flatten<T, TResult>(this IEnumerable<T> sequence, Func<T, Option<TResult>> map) =>
+            throw new NotImplementedException();
+    }
+}
