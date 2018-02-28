0993ccc55c7419ecc11573a21a900bf909ce273b
Johann <johann.vanbeveren@gmail.com>
2018-02-28 12:30:44 +0100

    Add Option<T>() class

---
+++ Src/Rop.Tools/Option.cs
@@ -0,0 +1,33 @@
+﻿namespace Rop.Tools
+{
+    public class Option<T>
+    {
+        public static implicit operator Option<T>(T value) => 
+            new Some<T>(value);
+
+        public static implicit operator Option<T>(None none) =>
+            new None<T>();
+    }
+
+    public class Some<T> : Option<T>
+    {
+        private readonly T _content;
+
+        public Some(T content)
+        {
+            _content = content;
+        }
+
+        public static implicit operator T(Some<T> value) => 
+            value._content;
+    }
+
+    public class None<T> : Option<T>
+    { }
+
+    public class None
+    {
+        public static None Value { get; } = new None();
+        private None() { }
+    }
+}
