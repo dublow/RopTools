c5d2a9905380b4bacd4663e3c194db7c7c2246d3
Johann <johann.vanbeveren@gmail.com>
2018-02-28 15:46:24 +0100

    GreenTest: ShouldMapEitherToLeft

--- Src/Rop.Tools/EitherExtensions.cs
+++ Src/Rop.Tools/EitherExtensions.cs
@@ -5,6 +5,8 @@ namespace Rop.Tools
     public static class EitherExtensions
     {
         public static Either<TLeft, TNewRight> Map<TLeft, TRight, TNewRight>(this Either<TLeft, TRight> either, Func<TRight, TNewRight> map) =>
-            throw new NotImplementedException();
+            either is Right<TLeft, TRight> right
+                ? (Either<TLeft, TNewRight>)map(right)
+                : (TLeft)(Left<TLeft, TRight>)either;
     }
 }
