3ec30ad890fbfe007336c1baba511c9c3b74a1f1
Johann <johann.vanbeveren@gmail.com>
2018-02-28 15:42:16 +0100

    RedTest: ShouldMapEitherToLeft

---
+++ Src/Rop.Tools.Tests/EitherExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace Rop.Tools.Tests
+{
+    [TestFixture]
+    public class EitherExtensionsTests
+    {
+        [Test]
+        public void ShouldMapEitherToLeft()
+        {
+            Either<Error, User> either = (Left<Error, User>)new Error("Error message");
+
+            Either<Error, string> actual = either.Map(user => user.Name);
+
+            Check.That(actual).InheritsFrom<Either<Error, string>>();
+            Check.That(actual).InheritsFrom<Left<Error, string>>();
+        }
+
+        private class User
+        {
+            public string Name { get; }
+
+            public User(string name)
+            {
+                Name = name;
+            }
+        }
+
+        private class Error
+        {
+            public string Message { get; }
+
+            public Error(string message)
+            {
+                Message = message;
+            }
+        }
+    }
+
+
+}
---
+++ Src/Rop.Tools/EitherExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Rop.Tools
+{
+    public static class EitherExtensions
+    {
+        public static Either<TLeft, TNewRight> Map<TLeft, TRight, TNewRight>(this Either<TLeft, TRight> either, Func<TRight, TNewRight> map) =>
+            throw new NotImplementedException();
+    }
+}
