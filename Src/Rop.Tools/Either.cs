bd9b0d0d0a5d5d73bb451fc280b3e27b5e593045
Johann <johann.vanbeveren@gmail.com>
2018-02-28 15:16:37 +0100

    Add Either class

---
+++ Src/Rop.Tools/Either.cs
@@ -0,0 +1,37 @@
+﻿namespace Rop.Tools
+{
+    public class Either<TLeft, TRight>
+    {
+        public static implicit operator Either<TLeft, TRight>(TLeft left) =>
+            new Left<TLeft, TRight>(left);
+
+        public static implicit operator Either<TLeft, TRight>(TRight right) =>
+            new Right<TLeft, TRight>(right);
+    }
+
+    public class Left<TLeft, TRight> : Either<TLeft, TRight>
+    {
+        private readonly TLeft _content;
+
+        public Left(TLeft content)
+        {
+            _content = content;
+        }
+
+        public static implicit operator TLeft(Left<TLeft, TRight> left) =>
+            left._content;
+    }
+
+    public class Right<TLeft, TRight> : Either<TLeft, TRight>
+    {
+        private readonly TRight _content;
+
+        public Right(TRight content)
+        {
+            _content = content;
+        }
+
+        public static implicit operator TRight(Right<TLeft, TRight> right) =>
+            right._content;
+    }
+}
